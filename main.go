b2cf9094bacdee8e71d2d5d9aee30f9df23e1f47
calzheng <calzheng@umich.edu>
2020-08-01 23:17:34 -0400

    Stylesheet added and included

--- main.go
+++ main.go
@@ -17,6 +17,9 @@ func main() {
 
 	mux := http.NewServeMux() // helps to call the correct handler based on the URL
 
+	fs := http.FileServer(http.Dir("assets"))  // put static files into server (i.e. styles.css)
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
 	mux.HandleFunc("/", indexHandler) // what function to call on main page
 	http.ListenAndServe(":3000", mux) // start a local server to run files
 }
